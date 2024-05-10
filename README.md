e5a7d04248b5c97590cce5e041c4acd93de96ffc
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2024-05-10 22:42:54 +0300

    docs: initial docs updated

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ In a simple terms, app will only be able to view messages that are sent to #jobs
 To be able to read messages from the channel we will need following permissions
 * **channels:read** -> This scope lets your app retrieve a list of all the public channels in a workspace so that we can pick one to retrieve a message from.
 * **channels:history** -> This scope lets your app view all the messages within any public channel in a workspace.
+
--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import os
 
 sys.path.insert(0, f"{os.getcwd()}/slack_jobs")
 from slack_jobs.slack import get_conversation_history
-from slack_jobs.config import CHANNEL_ID
+from slack_jobs.config import CHANNEL_ID, SLACK_BOT_TOKEN
 from slack_jobs.converter import json_to_df
 
 
 async def main():
-    rv = await get_conversation_history(channel_id=CHANNEL_ID)
+    rv = await get_conversation_history(
+        channel_id=CHANNEL_ID, bearer_token=SLACK_BOT_TOKEN
+    )
     df = json_to_df(rv)
-    print(df)
+    df.to_csv("jobs.csv", sep="~")
 
 
 if __name__ == "__main__":
