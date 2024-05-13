c4b4115219c198601d87e1edf9316755eb208848
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2024-05-13 23:50:28 +0300

    feature: app initialized

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ sys.path.insert(0, f"{os.getcwd()}/slack_jobs")
 from slack_jobs.slack import get_conversation_history
 from slack_jobs.config import CHANNEL_ID, SLACK_BOT_TOKEN
 from slack_jobs.converter import json_to_df
+from slack_jobs.utils import get_job_url, prepare_slack_message, write_to_file
+
+CWD = os.getcwd()
 
 
 async def main():
@@ -13,7 +16,9 @@ async def main():
         channel_id=CHANNEL_ID, bearer_token=SLACK_BOT_TOKEN
     )
     df = json_to_df(rv)
-    df.to_csv("jobs.csv", sep="~")
+    df["url"] = df["slack_message"].apply(get_job_url)
+    final_message = prepare_slack_message(df=df)
+    write_to_file(file_name=f"{CWD}/docs/index.md", data=final_message)
 
 
 if __name__ == "__main__":
--- slack_jobs/utils.py
+++ slack_jobs/utils.py
@@ -8,3 +8,23 @@ def get_job_url(text):
     url = url.split("|")[0] if len(url.split("|")) > 0 else url
     url = url[7:] if "mailto:" in url else url
     return url
+
+
+def prepare_slack_message(df):
+    main_text = ""
+    main_text += "# DataTalks.Club Job Postings\n"
+    divider = " ".join([" 🌐 " for i in range(3)]) + "\n"
+    for i, row in df.iterrows():
+        if row["url"] and row["slack_message_id"] and row["slack_message"]:
+            url = row["url"] + "\n"
+            message = row["slack_message"] + "\n"
+            main_text += divider + " JOB ALERT " + divider + "\n"
+            main_text += url + "\n"
+            main_text += message + "\n"
+    return main_text
+
+
+def write_to_file(file_name, data):
+    with open(file=file_name, mode="w") as file:
+        file.write(data)
+        return True
